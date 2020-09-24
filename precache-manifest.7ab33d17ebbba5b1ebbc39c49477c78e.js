7e621bc46bd56052ebff15813031eeaa276ae27b
Long Tran <long.tran2@ucalgary.ca>
2020-09-24 02:49:51 -0600

    Updates

---
+++ precache-manifest.7ab33d17ebbba5b1ebbc39c49477c78e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e4ac43f174727e3a24985b8b9a9cd5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70ee937d71a5c756508c",
+    "url": "/portfolio/static/css/main.31e2c544.chunk.css"
+  },
+  {
+    "revision": "ce67534c1025b9dce9df",
+    "url": "/portfolio/static/js/2.fc2bc0f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.fc2bc0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ee937d71a5c756508c",
+    "url": "/portfolio/static/js/main.495e41fb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e3047e42cb433f5717aacc477274b206",
+    "url": "/portfolio/static/media/profile.e3047e42.png"
+  },
+  {
+    "revision": "13136be556eb29fb45abcbd99be35061",
+    "url": "/portfolio/static/media/project1.13136be5.png"
+  },
+  {
+    "revision": "39eba2729ab76bdfddacd2d4d756b8c6",
+    "url": "/portfolio/static/media/project2.39eba272.png"
+  },
+  {
+    "revision": "4f5efae31d0389b6f06e3bb1cc532dea",
+    "url": "/portfolio/static/media/project3.4f5efae3.png"
+  }
+]);
